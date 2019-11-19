9e1454ebeb1e8d4bdddca2ddf7a23350887f839c
Scott Carnahan <scott.carnahan@colorado.edu>
2019-11-18 20:08:58 -0500

    refactor to use framework/etc for framework stuff
    also, now can read and write temperature message in scenario!!

--- src/models/test_thermalConductor.py
+++ src/models/test_thermalConductor.py
@@ -1,5 +1,17 @@
-import basilisk
 import thermal
 
-print(thermal.ThermalConductor().readUpstreamTemperature())
+def test_thermalConductor():
+    cond = thermal.ThermalConductor()
+    UT = thermal.TemperatureMsgClass()
+    UTData = thermal.TemperatureMsg()
+    UTData.temperature = 100
+    UTw = UT.add_author()
+    UTw(UTData)
+    UTr = UT.add_subscriber()
+    DT = thermal.TemperatureMsgClass()
+    DTw = DT.add_author()
+    assert 1==1
+
+if __name__ == "__main__":
+    test_thermalConductor()
 
--- src/thermal.i
+++ src/thermal.i
@@ -1,14 +1,15 @@
 %module(package="basilisk.thermal") thermal
-%ignore SysModel;
+%include "std_vector.i"
+%include "framework/messaging.i"
 %{
 #include "framework/system_models/sys_model.h"
 #include "models/thermalConductor.h"
 #include "thermalTypes.h"
-#include "messages/TemperatureMsg.h"
 %}
 %include "framework/system_models/sys_model.h"
 %include "models/thermalConductor.h"
 %include "thermalTypes.h"
-%include "messages/TemperatureMsg.h"
+
+INSTANTIATE_MESSAGE_TEMPLATE(TemperatureMsg)
 
 
