1a84f7daa8665a0bcb79f1af9e32f81ed7ab85ba
Scott Carnahan <scott.carnahan@colorado.edu>
2019-11-18 22:49:06 -0500

    thermal conductor test passing

--- src/models/test_thermalConductor.py
+++ src/models/test_thermalConductor.py
@@ -1,17 +1,38 @@
 import thermal
+import pytest
 
-def test_thermalConductor():
-    cond = thermal.ThermalConductor()
+def test_thermalConductorHeatRate():
+    """
+    This test checks that the heat rate across a conductor is calculated appropriately
+
+    It uses user-set messages to simulate up and downstream temperature messages
+    The conductance is set to a non-default value
+    The output is calculated independently and compared
+    """
     UT = thermal.TemperatureMsgClass()
     UTData = thermal.TemperatureMsg()
     UTData.temperature = 100
-    UTw = UT.add_author()
+    UTw = UT.get_writer()
     UTw(UTData)
-    UTr = UT.add_subscriber()
+    UTr = UT.get_reader()
+
     DT = thermal.TemperatureMsgClass()
-    DTw = DT.add_author()
-    assert 1==1
+    DTw = DT.get_writer()
+    DTData = thermal.TemperatureMsg()
+    DTData.temperature = 50
+    DTw(DTData)
+    DTr = DT.get_reader()
+
+    cond = thermal.ThermalConductor()
+    cond.conductance = 2.0
+    cond.readUpstreamTemperature = UT.get_reader()
+    cond.readDownstreamTemperature = DT.get_reader()
+    cr = cond.heatRateMsg.get_reader()
+    cond.UpdateState(0)
+
+    expected_rate = (UTr().temperature - DTr().temperature) * cond.conductance
+    assert cr().heatRate == pytest.approx(expected_rate, rel=1e-8)
+
 
 if __name__ == "__main__":
-    test_thermalConductor()
-
+    test_thermalConductorHeatRate()
